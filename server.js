93251dbe0c5f99c43346c7cfa93e90aa00d3c348
itachi880 <142436626+itachi880@users.noreply.github.com>
2025-01-12 15:38:51 +0100

    remove server.js file and its associated code

--- server.js
+++
@@ -1,9 +0,0 @@
-const Express = require("express");
-const server = Express();
-const fs = require("fs");
-const cors = require("cors");
-server.use(cors());
-server.get("/:name", (req, res) => {
-  res.sendFile(__dirname + "/" + req.params.name);
-});
-server.listen(3001);
